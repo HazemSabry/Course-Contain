7b532a077c6678cdd65a70e9eb2e422ee797dfa7
Hazem Sabry <89419013+HazemSabry@users.noreply.github.com>
2024-09-18 22:00:47 +0300

    update

---
+++ nhpaac/PaaC/paac-java-jtest-pipeline.groovy
@@ -0,0 +1,33 @@
+pipeline {
+  // This pipeline requires the following plugins:
+  // * Git: https://plugins.jenkins.io/git/
+  // * Workflow Aggregator: https://plugins.jenkins.io/workflow-aggregator/
+  // * JUnit: https://plugins.jenkins.io/junit/
+  //Guys change the branch to 'failing-test' to fail the test!! (Compare goood branch with bad one! before merging!)
+  agent 'any'
+  stages {
+    stage('Checkout') {
+      steps {
+        script {
+            checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/XP2600-hub/nh-java-testme.git']]])
+        }
+      }
+    }
+    stage('Test') {
+      steps {
+        sh(script: './mvnw --batch-mode -Dmaven.test.failure.ignore=true test')
+
+      }
+    }
+    stage('Package') {
+      steps {
+        sh(script: './mvnw --batch-mode package -DskipTests')
+      }
+    }
+  }
+  post {
+    always {
+      junit(testResults: 'target/surefire-reports/*.xml', allowEmptyResults : true)
+    }
+  }
+}
